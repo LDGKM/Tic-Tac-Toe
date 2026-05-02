27ba9e3da2b77d9f3d0055bbeb982e57ec2843a4
louto <loutoudeogratias@gmail.com>
2026-05-02 14:13:03 +0200

    Ajout du front pour l'index

--- Tic_Tac_Toe/resources/views/tic_tac_toe/index.blade.php
+++ Tic_Tac_Toe/resources/views/tic_tac_toe/index.blade.php
@@ -1,52 +1,49 @@
 <!DOCTYPE html>
-<html>
-
-<style>
-    table, th, td {
-        border: 1px solid black;
-        border-collapse: collapse;
-    }
-</style>
-
-
+<html lang="fr">
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Tic Tac Toe</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Tic Tac Toe</title>
+    @vite(['resources/css/app.css'])
 </head>
 <body>
-	@if($gagne != 'Victoire')
-	<p>{{ $gagne }}</p>
-	<table>
-		@for($i=0;$i<3;$i++)
-		<tr>
-		@foreach($case[$i] as $key=>$value)
-			<td>
-				@if(!$value[0])
-				<form action="{{ route('jeu.marquer') }}" method="POST">
-				@csrf
-				<a href=>
-					<button type="submit" name="case" value="{{ $key }}">
-					</button>
-				</a>
-				@else
-				<p>{{$value[1]}}</p>
-				@endif
-				</form>
-			</td>
-		
-		@endforeach
-		</tr>
-		@endfor
-	</table>
-	@else
-	<p>{{ $gagne }}</p>
-	@endif
 
-	<form action="{{ route('jeu.reinitialiser') }}" method="POST">
-		@csrf
-		<button type="submit">Reinitialiser</button>
-	</form>
+    <h1 class="ttt-title">Tic Tac Toe</h1>
+
+    <p class="ttt-status {{ $gagne === 'Victoire' ? 'victoire' : '' }}">
+        {{ $gagne }}
+    </p>
+
+    @if($gagne !== 'Victoire')
+    <div class="ttt-scene">
+        <table class="ttt-board">
+            @for($i = 0; $i < 3; $i++)
+            <tr>
+                @foreach($case[$i] as $key => $value)
+                <td>
+                    @if(!$value[0])
+                        <form action="{{ route('jeu.marquer') }}" method="POST">
+                            @csrf
+                            <button type="submit" name="case" value="{{ $key }}"></button>
+                        </form>
+                    @else
+                        {{-- Ajoutez mark-x ou mark-o selon votre logique --}}
+                        <p class="mark-{{ strtolower($value[1]) }}">{{ $value[1] }}</p>
+                    @endif
+                </td>
+                @endforeach
+            </tr>
+            @endfor
+        </table>
+    </div>
+    @endif
+
+    <div class="ttt-reset">
+        <form action="{{ route('jeu.reinitialiser') }}" method="POST">
+            @csrf
+            <button type="submit">↺ Réinitialiser</button>
+        </form>
+    </div>
 
 </body>
 </html>
